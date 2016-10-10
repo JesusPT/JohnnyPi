3c0615cca6d9c4add1325c71c6131a3a5e6719bd
root <shini.p.t.sis@gmail.com>
2016-10-10 18:21:31 -0500

    Prueba

--- ledPi.js
+++ ledPi.js
@@ -5,7 +5,7 @@ var board = new five.Board({
 });
 
 board.on('ready', function() {
-  led = new five.Led('P1-7');
+  led = new five.Led('P1-12');
   // Create an Led on pin 7 (GPIO4) on P1 and strobe it on/off
   // Optionally set the speed; defaults to 100ms
   led.pulse();
